5bc639f0677a4507f95a3464c252f67d90a39b33
anggiwdntr29 <widiantara2906@gmail.com>
2023-01-15 23:40:04 +0800

    responsive ke mobile

--- src/data/DataPortfolio.js
+++ src/data/DataPortfolio.js
@@ -7,6 +7,14 @@ const DataPortfolio = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     link: "/",
   },
+  {
+    id: 1,
+    src: "portfolio/sampel3.png",
+    judul: "X NFT",
+    deskripsi:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    link: "/",
+  },
 ];
 
 export default DataPortfolio;
