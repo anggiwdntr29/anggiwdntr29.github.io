f27db4d0148f3ee1e85ce76a06fc75b5e9a26390
anggiwdntr29 <widiantara2906@gmail.com>
2023-01-17 11:03:10 +0800

    add porto

--- src/data/DataPortfolio.js
+++ src/data/DataPortfolio.js
@@ -1,15 +1,6 @@
 const DataPortfolio = [
   {
     id: 1,
-    src: "anggi-movie.jpg",
-    judul: "Anggi Movie",
-    deskripsi: "Pencarian Film dengan API The Movie DB scr dea afrizal",
-    kategori1: "React",
-    kategori2: "Project Iseng",
-    link: "https://anggiwdntr29.github.io/pencarianfilm1/",
-  },
-  {
-    id: 2,
     src: "kelazer.jpg",
     judul: "Search Engine",
     deskripsi: "Search Engine Karya Anak Bangsa",
